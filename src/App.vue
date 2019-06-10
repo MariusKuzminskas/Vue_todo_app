6d4fc8c940bac1e861ccb78a4ab8cba8bf22650a
marius <marius752@gmail.com>
2019-06-10 18:30:02 +0300

    Add todo component added and working, uuid installed and working

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="app">
-        <h1>To Do APP</h1>
+        <Header />
+        <AddTodo v-on:add-todo="addTodo" />
         <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+        
   </div>
 </template>
 
 <script>
 import Todos from "./components/Todos";
+import Header from "./components/layout/Header";
+import AddTodo from "./components/AddTodo";
 
 
 export default {
   name: 'app',
   components: {
-    Todos
+    Todos, Header, AddTodo
   },
   data() {
     return{
@@ -38,6 +42,9 @@ export default {
   methods:{
     deleteTodo (id) {
       this.todos = this.todos.filter(todo => todo.id !== id)
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
     }
   }
 }
@@ -53,5 +60,16 @@ export default {
     font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
     line-height: 1.4;
 
+  }
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background-color: #666;
   }
 </style>
